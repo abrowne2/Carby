a3579732b402fb6a8ce31777ea9928a83fff26c0
Adam Browne <adamo.browne@gmail.com>
2016-10-15 21:50:52 -0700

    update flask framework

--- carby/main.py
+++ carby/main.py
@@ -6,18 +6,10 @@ import config, json, traceback, requests
 
 main = Blueprint('main', __name__)
 
-@main.route('/', methods=['GET', 'POST'])
+@main.route('/api', methods=['GET'])
 def main_route():
-    if request.method == 'POST':
-        try:
-            payload = request.get_json()
-            print(payload)
-            #parse the payload below, and then we'll use our 'computation' module
-
-            return "okay"
-        except Exception as e:
-            print(type(e))
-            print(e.args)
-
-    elif request.method == 'GET':
-        return "Yeah, this is a get request"
+    #only handle get requests.
+    if request.method == 'GET':
+        origin = request.args.get('origin')
+        destination = request.args.get('destination')
+        return "Origin: " + origin + ', Destination: ' + destination
