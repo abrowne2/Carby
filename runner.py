4ec145da54281a88b4ecfeeece4077348ea97f91
Adam Browne <adamo.browne@gmail.com>
2016-10-16 03:40:47 -0700

    fix shot caller

--- runner.py
+++ runner.py
@@ -1,4 +1,6 @@
-from algorithm.algo import shotCaller
+import sys
+sys.path.insert(0, 'algorithm/')
+from algorithm import algo
 
 def returnJSON(orig, dest):
-    return shotCaller(orig, dest)
+    return algo.shotCaller(orig, dest)
